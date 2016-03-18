77926948b7b426d8abce00509f3735961f4fb10c
Verachad Wongsawangtham <birth24987@gmail.com>
2016-03-18 14:41:48 +0700

    Change to use SSLv3 protocal

--- gear/socketio/internal/sio_client_impl.cpp
+++ gear/socketio/internal/sio_client_impl.cpp
@@ -477,7 +477,7 @@ void client_impl::reset_states() {
 }
 
 client_impl::context_ptr client_impl::on_tls_init(connection_hdl /*conn*/) {
-  context_ptr ctx = context_ptr(new asio::ssl::context(asio::ssl::context::tlsv12));
+  context_ptr ctx = context_ptr(new asio::ssl::context(asio::ssl::context::sslv3));
   std::error_code ec;
   ctx->set_options(asio::ssl::context::default_workarounds | asio::ssl::context::no_sslv2 |
                        asio::ssl::context::single_dh_use,
